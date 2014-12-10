2a10767140d94e5031c0bb579a540216da46fcf0
mickpc <mpc201@bigpond.com>
2014-12-10 19:46:25 +1100

    changed message

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    return HttpResponse("mick says hello world!")
 
 # Create your views here.
