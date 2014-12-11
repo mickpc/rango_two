0df4d9479036e2e4abe8673b1ab00240fef5fb63
mickpc <mpc201@bigpond.com>
2014-12-12 08:59:41 +1100

    added about page

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -18,4 +18,6 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
 
     url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
+    url(r'^about/', include('about.urls')), # ADD THIS NEW TUPLE!
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
